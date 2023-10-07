86c6db91650febe6ab1c06e88961a76d0b68fc56
edel1105 <140633425+edel1105@users.noreply.github.com>
2023-10-07 13:45:42 +0530

    Add files via upload

--- 13_21_edel.c
+++ 13_21_edel.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
 void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
             }
         }
     }
 }
 
 int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
     printf("Array before sorting:\n");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
-
+    }
+    printf("\n");
+    
     bubbleSort(arr, n);
-
-    printf("\nArray after sorting:\n");
-    for (int i = 0; i < n; i++)
+    
+    printf("Array after sorting:\n");
+    for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
-
+    }
+    printf("\n");
+    
     return 0;
 }
-    
